996ede22cfdb72c4042c978929c3606794144ba9
Alexander von Gluck IV <kallisti5@unixzen.com>
2014-11-21 12:11:07 -0600

    Update changelog, fix headers

--- src/PCEngine.cpp
+++ src/PCEngine.cpp
@@ -1,3 +1,14 @@
+/*****************************************************
+ * HuExpress, the PC Engine emulator
+ *
+ * Copyright 2011-2013 Alexander von Gluck IV
+ * Released under the terms of the GPLv2 license.
+ *
+ * Original HuGO! Authors:
+ *	Copyright 2001-2005 Zeograd
+ *  Copyright 1998 BERO
+ *  Copyright 1996 Alex Krasivsky, Marat Fayzullin
+ */
 
 
 #include "PCEngine.h"
